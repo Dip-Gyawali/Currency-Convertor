bfb1e9f77138d2c1d0ff28e9a8e71ab83725a834
Hiten <hitengamer11@gmail.com>
2024-08-16 22:11:27 +0545

    Api added and conversion occured successfully

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,4 +1,72 @@
+import { useState } from "react";
+
 export default function Content() {
+    let currencies = [
+        "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN",
+        "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL",
+        "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLP", "CNY",
+        "COP", "CRC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP",
+        "ERN", "ETB", "EUR", "FJD", "FKP", "FOK", "GBP", "GEL", "GGP", "GHS",
+        "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF",
+        "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD",
+        "JPY", "KES", "KGS", "KHR", "KID", "KMF", "KRW", "KWD", "KYD", "KZT",
+        "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD",
+        "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN",
+        "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK",
+        "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR",
+        "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLE", "SLL", "SOS", "SRD",
+        "SSP", "STN", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY",
+        "TTD", "TVD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VES",
+        "VND", "VUV", "WST", "XAF", "XCD", "XOF", "XPF", "YER", "ZAR", "ZMW",
+        "ZWL"
+    ];
+    const [conversionAmt,setConversionAmt]=useState('');
+    const [fromCurr,setFromCurr]=useState('USD');
+    const [toCurr,setToCurr]=useState('NPR');
+    const [conversionRate,setConversionRate]=useState(0);
+    const [startCode,setStartCode]=useState(false);
+
+    let apiKey = import.meta.env.VITE_API_KEY;
+    let apiUrl= `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${fromCurr}/${toCurr}`;
+
+    function handleFrom(e){
+        setFromCurr(e.target.value);
+    }
+
+    function handleTo(e){
+        setToCurr(e.target.value);
+    }
+
+    function handleConversionAmt(e){
+        setConversionAmt(e.target.value);
+    }
+ 
+    useState(()=>{
+        if(startCode){
+            getData();
+        }
+    },[])
+
+    async function getData(){
+        setStartCode(true);
+        
+        try{
+            let response = await fetch(apiUrl);
+            if(!response.ok){
+                console.log("error");
+            }
+            // console.log(response);
+            let data = await response.json();
+            console.log(data);
+            console.log(data.conversion_rate  * conversionAmt);
+            setConversionRate((data.conversion_rate * conversionAmt).toFixed(2));
+
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
     return (
         <>
             <div className="home">
@@ -6,33 +74,34 @@ export default function Content() {
                 <div className="convert-sec">
                     <div className="from-sec">
                         <h1 className="from-head">From :</h1>
-                        <select className="drop-down">
-                            <option value="USD" selected>USD</option>
-                            <option value="NPR">NPR</option>
-                            <option value="INR">INR</option>
+                        <select className="drop-down" onChange={(e)=> handleFrom(e)} value={fromCurr}>
+                            {currencies.map((ele)=>(
+                                <option value={ele} key={ele}>{ele}</option>
+                            ))}
                         </select>
                         <div className="conversion">
-                            <img src="https://flagsapi.com/US/flat/64.png" alt="flag" />
-                            <input type="number" placeholder="Enter Price" required />
+                            <img src={`https://flagsapi.com/${fromCurr.slice(0,2)}/flat/64.png`} alt="flag" />
+                            <input type="number" placeholder="Enter Price" value={conversionAmt} onChange={(e)=> handleConversionAmt(e)} required />
                         </div>
                     </div>
                     <div className="to-sec">
                         <h1 className="to-head">To :</h1>
-                        <select className="drop-down">
-                            <option value="USD">USD</option>
-                            <option value="NPR" selected>NPR</option>
-                            <option value="INR">INR</option>
+                        <select className="drop-down"  onChange={(e)=> handleTo(e)} value={toCurr}>
+                            {currencies.map((ele)=>(
+                                <option value={ele} key={ele}>{ele}</option>
+                            ))}
                         </select>
                         <div className="conversion">
-                            <img src="https://flagsapi.com/IN/flat/64.png" alt="flag" />
-                            <h1 className="result">400</h1>
+                            <img src={`https://flagsapi.com/${toCurr.slice(0,2)}/flat/64.png`} alt="flag" />
+                            <h1 className="result">{conversionRate}</h1>
                         </div>
                     </div>
                 </div>
                 <div className="btn-sec">
-                    <button>Convert</button>
+                    <button onClick={getData}>Convert</button>
                 </div>
             </div>
+            {/* <h1>{fromCurr} {toCurr}</h1> */}
         </>
     );
 }
